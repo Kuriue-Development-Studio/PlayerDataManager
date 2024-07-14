d568cf92e19368e18d1a71d44464c259069868b6
BuildTools <unconfigured@null.spigotmc.org>
2024-07-14 16:47:36 +0900

    update 1.4

--- src/main/java/util/ItemList.java
+++ src/main/java/util/ItemList.java
@@ -3,13 +3,19 @@ package util;
 import org.bukkit.Material;
 import org.bukkit.entity.Item;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 public class ItemList {
 
     public final static ItemStack air;
+    public final static ItemStack x;
 
     static {
         air = new ItemStack(Material.AIR);
+        x = new ItemStack(Material.BARRIER);
+        ItemMeta m = x.getItemMeta();
+        m.setDisplayName("§cX");
+        x.setItemMeta(m);
     }
 
 }
